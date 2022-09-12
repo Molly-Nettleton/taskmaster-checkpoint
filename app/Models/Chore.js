9fd18c1157f158afcde3ca51a6e95eb85eea19f5
Molly-Nettleton <mollyelainen@gmail.com>
2022-09-11 22:06:54 -0600

    Mobile friendly

--- app/Models/Chore.js
+++ app/Models/Chore.js
@@ -4,13 +4,14 @@ import { generateId } from "../Utils/generateId.js";
 export class Chore {
 
   /**
-   * @param {{chorename: string, choreComplete: string, id:string, cardId:string}} data 
+   * @param {{chorename: string, choreComplete: string, id:string, cardId:string, color: string,}} data 
    */
   constructor(data) {
     this.id = data.id || generateId()
     this.chorename = data.chorename
     this.choreComplete = data.choreComplete || false
     this.cardId = data.cardId
+    this.color = data.color
 }
 
   get ChoreTemplate() {
@@ -18,7 +19,7 @@ export class Chore {
     <div class="d-flex justify-content-between align-items-baseline">
                 <input onchange="app.choresController.toggleChoreComplete('${this.id}')" class="form-check-input" ${this.choreComplete ? 'checked' : ''} type="checkbox" value="">
                 
-                <p class="p-2 mb-2 border-bottom border-2 border-info">${this.chorename}</p>
+                <p class="p-2 mb-2 border-bottom border-2 border-secondary">${this.chorename}</p>
                 <i onclick="app.choresController.deleteChore('${this.id}')" class="mdi mdi-close selectable"></i>
               </div>
     `
